21b278c864d3debebefbc2855076cc40c675e393
Spencer Florence <spencer@florence.io>
2016-04-15 19:35:30 -0500

    moved rackunit/log to new pkg, fixed docs

---
+++ testing-util-lib/info.rkt
@@ -0,0 +1,10 @@
+#lang info
+(define collection 'multi)
+
+(define deps '("base"))
+
+(define pkg-desc "Utilities for interoperating between testing frameworks")
+
+(define version "1.0")
+
+(define pkg-authors '(florence))
