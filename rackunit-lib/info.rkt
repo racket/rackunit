c3ef801b2d4f610483135154fa0df911db329ff9
Jack Firth <jackhfirth@gmail.com>
2017-07-06 01:37:30 -0700

    Fix package dependency change

--- rackunit-lib/info.rkt
+++ rackunit-lib/info.rkt
@@ -3,9 +3,9 @@
 (define collection 'multi)
 
 (define deps '("base"
-               "testing-util-lib"))
+               ("testing-util-lib" #:version "1.1")))
 
-(define implies '(("testing-util-lib" #:version "1.1")))
+(define implies '("testing-util-lib"))
 
 (define pkg-desc "RackUnit testing framework")
 
