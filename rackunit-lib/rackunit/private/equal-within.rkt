044dd42d1243977a657eecd830e09c9b8a6ac7c8
AlexKnauth <alexander@knauth.org>
2018-08-11 20:04:55 -0400

    check-within goes within extflvectors

--- rackunit-lib/rackunit/private/equal-within.rkt
+++ rackunit-lib/rackunit/private/equal-within.rkt
@@ -2,7 +2,7 @@
 
 (provide equal?/within)
 
-(require racket/flonum)
+(require racket/flonum racket/extflonum)
 
 ;; equal?/within : Any Any Nonnegative-Real -> Boolean
 (define (equal?/within a b delta)
@@ -11,9 +11,15 @@
     (cond
       [(and (number? a) (number? b))
        (<= (magnitude (- a b)) delta)]
+      [(and (extflonum? a) (extflonum? b))
+       (extfl<= (extflabs (extfl- a b)) (real->extfl delta))]
       [(and (flvector? a) (flvector? b))
        (and (= (flvector-length a) (flvector-length b))
             (for/and ([a (in-flvector a)] [b (in-flvector b)])
+              (equal-proc a b)))]
+      [(and (extflvector? a) (extflvector? b))
+       (and (= (extflvector-length a) (extflvector-length b))
+            (for/and ([a (in-extflvector a)] [b (in-extflvector b)])
               (equal-proc a b)))]
       [else
        (equal?/recur a b equal-proc)]))
